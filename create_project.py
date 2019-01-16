3d65e58706ba52822c81e8d39b4707dd1c0d9657
Mary <mgibbs1259@gmail.com>
2019-01-16 16:05:59 -0500

    automated file upload

--- create_project.py
+++ create_project.py
@@ -330,15 +330,13 @@ def validate_edge_build_version(driver):
     """ Compare selenium and requests edge build version """
     # set up
     CreateProject(driver)
-    #test_project.log_in()
-    #test_project.remove_guide()
     # switch to api/ping
     driver.get("http://localhost:10000/api/ping/")
     selenium_edge_build_version = json.loads(driver.find_element_by_css_selector("pre").text)
     assert selenium_edge_build_version == version_info, "selenium does not match requests edge build version"
 
 
-def drag_drop_file_local_browser(driver):
+def drag_drop_file_local_to_browser(driver):
     # set up
     test_project = CreateProject(driver)
     test_project.log_in()
@@ -353,7 +351,80 @@ def drag_drop_file_local_browser(driver):
     driver.find_element_by_css_selector("#inputData").click()
     time.sleep(3)
     # drag and drop from local to browser
+    js_script = """for (var b = arguments[0], k = arguments[1], l = arguments[2], c = b.ownerDocument, m = 0;;) {
+        var e = b.getBoundingClientRect(),
+            g = e.left + (k || e.width / 2),
+            h = e.top + (l || e.height / 2),
+            f = c.elementFromPoint(g, h);
+        if (f && b.contains(f)) break;
+        if (1 < ++m) throw b = Error('Element not interractable'), b.code = 15, b;
+        b.scrollIntoView({
+            behavior: 'instant',
+            block: 'center',
+            inline: 'center'
+        })
+        }
+    var a = c.createElement('INPUT');
+    a.setAttribute('type', 'file');
+    a.setAttribute('style', 'position:fixed;z-index:2147483647;left:0;top:0;');
+    a.onchange = function() {
+        var b = {
+            effectAllowed: 'all',
+            dropEffect: 'none',
+            types: ['Files'],
+            files: this.files,
+            setData: function() {},
+            getData: function() {},
+            clearData: function() {},
+            setDragImage: function() {}
+        };
+        window.DataTransferItemList && (b.items = Object.setPrototypeOf([Object.setPrototypeOf({
+            kind: 'file',
+            type: this.files[0].type,
+            file: this.files[0],
+            getAsFile: function() {
+                return this.file
+            },
+            getAsEntry: function() {
+                return this.file
+            },
+            getAsString: function(b) {
+                var a = new FileReader;
+                a.onload = function(a) {
+                    b(a.target.result)
+                };
+                a.readAsText(this.file)
+            }
+        }, DataTransferItem.prototype)], DataTransferItemList.prototype));
+        Object.setPrototypeOf(b, DataTransfer.prototype);
+        ['dragenter', 'dragover', 'drop'].forEach(function(a) {
+            var d = c.createEvent('DragEvent');
+            d.initMouseEvent(a, !0, !0, c.defaultView, 0, 0, 0, g, h, !1, !1, !1, !1, 0, null);
+            Object.setPrototypeOf(d, null);
+            d.dataTransfer = b;
+            Object.setPrototypeOf(d, DragEvent.prototype);
+            f.dispatchEvent(d)
+        });
+        a.parentElement.removeChild(a)
+    };
+    c.documentElement.appendChild(a);
+    a.getBoundingClientRect();
+    return a;"""
+    drop_target = driver.find_element_by_css_selector(".FileBrowser")
+    file_path = os.path.join(os.getcwd(), 'testmaterial/file-3000000b.rando')
+    drag_and_drop_file(drop_target, file_path, js_script)
+    time.sleep(3)
+    flat_dir_path = os.path.join(os.getcwd(), 'testmaterial/flatdir')
+    drag_and_drop_file(drop_target, flat_dir_path, js_script)
+    time.sleep(60)
 
+
+def drag_and_drop_file(drop_target, path, js_script):
+    #driver = drop_target.parent
+    file_input = driver.execute_script(js_script, drop_target, 0, 0)
+    file_input.send_keys(path)
+
+#drag_and_drop_file(drop_target, path)
 
 def test_example_success(driver):
     my_sum = 1 + 1
@@ -391,7 +462,7 @@ if __name__ == '__main__':
     tests_collection = {}
 
     # You may edit this as need-be
-    methods_under_test = []
+    methods_under_test = [drag_drop_file_local_to_browser]
 
     for test_method in methods_under_test:
         driver = testutils.load_chrome_driver()
