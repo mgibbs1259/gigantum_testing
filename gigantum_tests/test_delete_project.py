fdcdc9a7a47396f6459a55469b8fcf845bef716d
Mary <mgibbs1259@gmail.com>
2019-01-31 16:15:37 -0500

    test_delete_project

--- gigantum_tests/test_delete_project.py
+++ gigantum_tests/test_delete_project.py
@@ -13,14 +13,27 @@ from selenium.webdriver.common.action_chains import ActionChains
 import testutils
 
 
-
-def test_labbook_delete(driver: selenium.webdriver, *args, **kwargs):
-
-
+def test_delete_project(driver: selenium.webdriver, *args, **kwargs):
     testutils.log_in(driver)
     time.sleep(2)
     testutils.remove_guide(driver)
-    testutils.create_project_without_base(driver)
     time.sleep(2)
-    # python 3 minimal base
+    testutils.create_project_without_base(driver)
     testutils.add_py3_min_base(driver)
+    wait = WebDriverWait(driver, 200)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex>.Stopped")))
+    #grab project title
+    project_title = driver.find_element_by_css_selector(".LabbookHeader__section--title").text
+    symbol_index = project_title.index("/") + 1
+    project_title = project_title[symbol_index:]
+    print(project_title)
+    driver.find_element_by_css_selector(".BranchMenu__btn").click()
+    time.sleep(1)
+    driver.find_element_by_css_selector(".BranchMenu__item--delete").click()
+    time.sleep(1)
+    driver.find_element_by_css_selector("#deleteInput").send_keys(project_title)
+    driver.find_element_by_css_selector(".DeleteLabbook > .ButtonLoader").click()
+
+
+
+
