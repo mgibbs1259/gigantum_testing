85531b4148ff066d3ee94cfb839e036f33a0bc97
Bill Van Besien <vanbesien@gmail.com>
2019-02-07 11:06:02 -0500

    Issue #25 - Terminate old containers and clean workspace between tests (#39)

--- driver.py
+++ driver.py
@@ -1,4 +1,3 @@
-# Builtin imports
 import argparse
 import logging
 import pprint
@@ -8,7 +7,7 @@ import uuid
 import sys
 import os
 
-# Library imports
+import docker
 import selenium
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
@@ -17,7 +16,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
 
-# Local packages
 import testutils
 
 TEST_ROOT = os.path.join(os.getcwd(), 'gigantum_tests')
@@ -88,6 +86,17 @@ def run_playbook(path, headless, firefox):
     return test_collect
 
 
+def stop_project_containers(client):
+    containers = client.containers.list()
+    for c in containers:
+        for t in [c.image.tags]:
+            if 'gmlb-' in t:
+                logging.info(f"Stopping container for image {t}")
+                c.stop()
+    logging.info("Pruning all Docker containers")
+    logging.info(client.containers.prune())
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     argparser = argparse.ArgumentParser()
@@ -99,6 +108,9 @@ if __name__ == '__main__':
                            help='Optional name of specific playbook')
     args = argparser.parse_args()
 
+    # TODO - Remove this line shortly
+    os.environ['GIGANTUM_HOME'] = os.path.expanduser('~/gigantum/')
+    docker_client = docker.from_env()
     playbooks = get_playbooks(args.test_path)
 
     failed = False
@@ -108,6 +120,10 @@ if __name__ == '__main__':
         if any([r[l]['status'].lower() != 'pass' for l in r]):
             failed = True
         full_results[pb] = r
+        stop_project_containers(docker_client)
+    
+    logging.info("Cleaning up...")
+    testutils.cleanup()
 
     print(f'\n\nTEST SUMMARY ({len(full_results)} tests)\n')
     for test_file in full_results.keys():
